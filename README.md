5cc44b0eaacfaf0077ad921c594015ecdfc48d0b
xieqihuiPG <qihui@propertyguru.com.sg>
2018-05-22 11:08:28 +0800

    modify vim_setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Useful Tools to Setup Development Environment for Data Science
+
+## Vim for Python
+Vim is awesome but painful to configure. Here is the script to configure it for Python.
+```
+./vim_setup.sh
+```
--- vim_setup.sh
+++ vim_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Set up Vim for Python
 # Check VIM version
 vim --version
@@ -12,6 +14,10 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Copy vimrc file
 cp ./vimrc ~/.vimrc
+
+# Install pathogen.vim to mange `runtimepath` with ease
+mkdir -p ~/.vim/autoload && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install plugins specified in vimrc
 vim +PluginInstall +qall
