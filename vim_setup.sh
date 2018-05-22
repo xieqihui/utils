bfd1e7d085501a2fca6203eff96793e7f1a40233
xieqihuiPG <qihui@propertyguru.com.sg>
2018-05-22 11:22:34 +0800

    correct vim setup

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-# Set up Vim for Python
-# Check VIM version
+### Set up Vim for Python
+# Reference: https://realpython.com/vim-and-python-a-match-made-in-heaven/#vim-extensions
+## Check VIM version
 vim --version
 
-# Install VIM
+## Install VIM
 sudo apt-get remove vim-tiny
 sudo apt-get update
 sudo apt-get install vim
 
-# Install Vundle
+## Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Copy vimrc file
+## Copy vimrc file
 cp ./vimrc ~/.vimrc
 
-# Install pathogen.vim to mange `runtimepath` with ease
+## Install pathogen.vim to mange `runtimepath` with ease
 mkdir -p ~/.vim/autoload && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# Install plugins specified in vimrc
+## Install plugins specified in vimrc
 vim +PluginInstall +qall
+
+## Install YouCompleteMe
+# https://github.com/Valloric/YouCompleteMe#mac-os-x-super-quick-installation
+# Install development tools and CMake
+sudo apt-get install build-essential cmake
+# Make sure you have Python headers installed:
+sudo apt-get install python-dev python3-dev
+# Compile YCM without sematic support of C-family languages
+cd ~/.vim/bundle/YouCompleteMe
+./install.py
--- vimrc
+++ vimrc
@@ -54,7 +54,7 @@ set encoding=utf-8
 Bundle 'Valloric/YouCompleteMe'
 let g:ycm_autoclose_preview_window_after_completion=1
 map <leader>g  :YcmCompleter GoToDefinitionElseDeclaration<CR>
-let g:ycm_server_python_interpreter='/usr/bin/python3'
+let g:ycm_server_python_interpreter='/usr/bin/python'
 
 "python with virtualenv support
 py3 << EOF
